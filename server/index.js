3afe36651c3aeec360d3340468bf9e9ab3fb83de
zachariast <zachariastraianos@gmail.com>
2020-03-15 23:14:53 +0200

    Lint fix && env changes

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 
 const scapeTable = require('./scrapeTables')
 
-
 app.get('/', async (req, res) => {
-    try {
-        const data = await scapeTable();
-        res.json(data)    
-    } catch (error) {
-        console.log(error)
-    }
-    
+  try {
+    const data = await scapeTable()
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 app.listen(3001, () => {
-    console.log("http://localhost:3001")
+  console.log('http://localhost:3001')
 })
--- server/scrapeTables.js
+++ server/scrapeTables.js
@@ -1,40 +1,50 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+require('dotenv').config()
 
-const url = "https://www.worldometers.info/coronavirus/#countries";
+const url = process.env.SCRAPE_URL
 
-let cache = null;
-let lastCacheTime = null;
+let cache = null
+let lastCacheTime = null
 
 async function scrapeTable() {
-    if(cache && lastCacheTime > Date.now() - (1000 * 60 * 10)){
-        return cache;
-    }
-    const response = await fetch(url);
-    const html = await response.text();
-    const $ = cheerio.load(html);
-    const table = $("#main_table_countries");
-    const header = table.find("thead tr");
-    const headers = [];
-    header.find("th").each((i, element) => {
-        headers.push(
-        $(element).text().trim().replace(/\W/g, "_")
-        );
-    });
-    const rows = [];
-    $(table.find("tbody")[0]).find("tr").each((i, element) => {
-        const row = {}
-        $(element).find("td").each((i, column) => {
-            row[headers[i]] = $(column).text().trim()
-            if(i !== 0) {
-                row[headers[i]] = Number(row[headers[i]].replace(/\+|,/g, '') || 0)
-            }
-        });
-        rows.push(row)
-    });
-    cache = rows;
-    lastCacheTime = Date.now()
-    return rows
+  if (cache && lastCacheTime > Date.now() - 1000 * 60 * 10) {
+    return cache
+  }
+  const response = await fetch(url)
+  const html = await response.text()
+  const $ = cheerio.load(html)
+  const table = $('#main_table_countries')
+  const header = table.find('thead tr')
+  const headers = []
+  header.find('th').each((i, element) => {
+    headers.push(
+      $(element)
+        .text()
+        .trim()
+        .replace(/\W/g, '_')
+    )
+  })
+  const rows = []
+  $(table.find('tbody')[0])
+    .find('tr')
+    .each((i, element) => {
+      const row = {}
+      $(element)
+        .find('td')
+        .each((i, column) => {
+          row[headers[i]] = $(column)
+            .text()
+            .trim()
+          if (i !== 0) {
+            row[headers[i]] = Number(row[headers[i]].replace(/\+|,/g, '') || 0)
+          }
+        })
+      rows.push(row)
+    })
+  cache = rows
+  lastCacheTime = Date.now()
+  return rows
 }
 
-module.exports = scrapeTable;
+module.exports = scrapeTable
